fcabca01b19e416526a7889c3e208a3fe5ebd3d4
STRY2007 <shristisharma1819@gmail.com>
2025-09-15 15:23:18 +0530

    message

---
+++ day2/day5/index.js
@@ -0,0 +1,20 @@
+console.log("welcome to the FSD Session!");
+document.write("welcome");
+document.writeln("welcome to the FSD");
+alert("h")
+
+let a = prompt("enter value");
+var a=20;
+console.log(a);
+a=30;
+console.log(a);
+{
+    var a=40;
+    console.log(a);
+}
+fun(70)
+function fun(a)
+{
+    let a=90;
+    console.log(a);
+}
